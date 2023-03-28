56e65611088cf692b1ae33400c6ee2178de3d904
oshmos <ojames9707@gmail.com>
2023-03-28 12:30:52 +0900

    코드잇 DRF : 개발 환경 설정, movies 데이터 조회

---
+++ movies/views.py
@@ -0,0 +1,14 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Movie
+from .serializers import MovieSerializer
+
+# Create your views here.
+
+
+@api_view(['GET'])
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data, status=200)
