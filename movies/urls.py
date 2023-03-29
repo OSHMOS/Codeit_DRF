29ccadfb02eb59024ca9890c0a3bce1ea1f66bf3
oshmos <ojames9707@gmail.com>
2023-03-29 11:50:54 +0900

    코드잇 DRF : movies, actors 데이터 생성

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import movie_list, actor_list
 
 urlpatterns = [
-    path('movies', movie_list),
+    path('movies', movie_list, name='movies'),
     path('actors', actor_list),
 ]
