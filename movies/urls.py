9474704eb0e9a170e0caf9287fb516eb2503ee20
oshmos <ojames9707@gmail.com>
2023-04-04 09:22:16 +0900

    코드잇 DRF : movies GET-detail, PATCH, DELETE

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import movie_list, actor_list
+from .views import movie_list, movie_detail, actor_list
 
 urlpatterns = [
     path('movies', movie_list, name='movies'),
+    path('movies/<int:pk>', movie_detail),
     path('actors', actor_list),
 ]
